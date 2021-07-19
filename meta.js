3e10b13d546bd546627e9641d8e01f1c2a83378c
shibo <447351751@qq.com>
2021-07-19 16:51:03 +0800

    调整项目结构

---
+++ meta.js
@@ -0,0 +1,14 @@
+module.exports = {
+  prompts: {
+    description: {
+      type: 'string',
+      required: false,
+      message: '项目描述',
+      default: 'A mult-side project'
+    },
+    author: {
+      type: 'string',
+      message: '作者'
+    }
+  }
+};
--- src/btns/ripple-btn/index.js
+++
@@ -1,5 +0,0 @@
-import RippleBtn from './ripple-btn.vue'
-
-RippleBtn.install = Vue => Vue.component(RippleBtn.name, RippleBtn)
-
-export default RippleBtn
