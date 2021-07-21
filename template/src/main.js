2e02c9df8da10f2d04b822a40ee08d09415d856f
shibo <447351751@qq.com>
2021-07-21 14:25:53 +0800

    修复编译错误

--- template/src/main.js
+++ template/src/main.js
@@ -1,5 +1,13 @@
 import App from './App';
-{% if !vue3 %}
+{% if vue3 %}
+import { createApp } from 'vue';
+
+const app = createApp({
+  ...App
+});
+
+app.mount();
+{% else %}
 import Vue from 'vue';
 Vue.config.productionTip = false;
 App.mpType = 'app';
@@ -7,15 +15,11 @@ App.mpType = 'app';
 const app = new Vue({
   ...App
 });
-{% else %}
-import { createApp } from 'vue';
-
-const app = createApp({
-  ...App
-});
-{% endif %}
 
 app.$mount();
+{% endif %}
+
+
 
 
 
--- template/vue.config.js
+++ template/vue.config.js
@@ -1,7 +1,5 @@
-
 {% if vue3 %}
 process.env.UNI_USING_VUE3 = true
 process.env.UNI_USING_VUE3_OPTIONS_API = true
 {% endif %}
-
 module.exports = {}
