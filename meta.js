0452e04d55c31f65a0f0278067c013769586d9a5
shibo <447351751@qq.com>
2021-07-21 11:53:48 +0800

    添加配置Vue3

--- meta.js
+++ meta.js
@@ -9,6 +9,11 @@ module.exports = {
     author: {
       type: 'string',
       message: '作者'
+    },
+    vue3: {
+      type: 'confirm',
+      message: '是否使用Vue3,（仅支持小程序，详情：https://ask.dcloud.net.cn/article/37834）',
+      default: false
     }
   }
 };
---
+++ template/vue.config.js
@@ -0,0 +1,7 @@
+
+{% if vue3 %}
+process.env.UNI_USING_VUE3 = true
+process.env.UNI_USING_VUE3_OPTIONS_API = true
+{% endif %}
+
+module.exports = {}
