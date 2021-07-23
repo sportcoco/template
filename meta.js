1e94aca35d487f64f20f41566babcc4f9ee3daef
shibo <447351751@qq.com>
2021-07-23 11:23:18 +0800

    添加选择UI框架配置项

--- meta.js
+++ meta.js
@@ -12,8 +12,55 @@ module.exports = {
     },
     vue3: {
       type: 'confirm',
-      message: '是否使用Vue3,（仅支持小程序，详情：https://ask.dcloud.net.cn/article/37834）',
+      message: '是否使用 Vue3,（仅支持小程序，详情：https://ask.dcloud.net.cn/article/37834）',
       default: false
+    },
+    useUI: {
+      type: 'confirm',
+      message: '是否使用 UI 框架',
+      default: true
+    },
+    UI: {
+      when: 'useUI === true',
+      type: 'checkbox',
+      message: '请选择 UI 框架（多选）',
+      choices: (val) => {
+        const baseUI = [
+          {
+            name: 'uni-ui',
+            value: {
+              name: 'uniUI',
+              pkg: `"@dcloudio/uni-ui": "^1.3.7"`,
+              easycom: `"^uni-(.*)": "@dcloudio/uni-ui/lib/uni-$1/uni-$1.vue"`
+            }
+          }
+        ];
+        const uViewUI = [
+          {
+            name: 'uview-ui',
+            value: {
+              name: 'uViewUI',
+              pkg: `"uview-ui": "^1.8.4"`,
+              easycom: `"^u-(.*)": "uview-ui/components/u-$1/u-$1.vue"`
+            }
+          }
+        ];
+
+        return val.vue3 ? baseUI : baseUI.concat(uViewUI);
+      },
+      default: []
+    }
+  },
+  computed: {
+    uniUI() {
+      return this.UI.some((e) => e.name === 'uniUI');
+    },
+    uViewUI() {
+      return this.UI.some((e) => e.name === 'uViewUI');
+    },
+    useEasycom() {
+      const arr = this.UI.map((e) => e.easycom);
+      return `"easycom":{ ${arr.join(',')} }`;
     }
   }
 };
--- template/src/App.vue
+++ template/src/App.vue
@@ -12,6 +12,16 @@
 	}
 </script>
 
+{% if uViewUI %}
+<style lang="scss">
+	/*每个页面公共css */
+	@import "uview-ui/index.scss";
+</style>
+{% else %}
 <style>
 	/*每个页面公共css */
+	@import "colorui/colorui/main.css";
+	@import "colorui/colorui/icon.css";
+	@import "colorui/colorui/animation.css";
 </style>
+{% endif %}
